b1ab73f8aaeab58ad53531366d3b1e600c92866b
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-09-26 17:16:46 +0000

    python vers

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,19 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Liya Getachew
+Created on: Sep 2023
+This module is a Micro:bit MicroPython program that shows the temperature in Kelvin.
 """
 
 from microbit import *
 
+display.clear()
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+# variable
+temp = 0
+
+while True:
+    if button_a.is_pressed():
+        temp = temperature()
+        temp = temp + 273.15
+        display.scroll("The temperature is " + str(temp) + " K.")
