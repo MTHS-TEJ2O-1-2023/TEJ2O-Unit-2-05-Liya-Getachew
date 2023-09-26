e26b83c0a642997fa2cc1d73b567f7c975d7fa3e
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-09-26 12:54:18 -0400

    typescript vers

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Liya Getachew
+ * Created on: Sep 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+// variable
+let temperature: number
+
+input.onButtonPressed(Button.A, function () {
+  // input
+  temperature = input.temperature()
+
+  // process
+  temperature = temperature + 273.15
+  temperature =  Math.round(temperature)
+
+  // output
+  basic.showString('The temperature is ' + temperature.toString() + 'K.')
+})
